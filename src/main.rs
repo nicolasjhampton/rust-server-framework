670375c8482d5a322c656ce4cc140577a9a9e9ba
Nic Hampton <nicolasjhampton@gmail.com>
2020-06-06 20:22:26 -0700

    Connection reattempts handled

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,20 @@
 use std::net::TcpListener;
+use retry::retry_with_index;
+use retry::delay::Fixed;
 
 fn main() {
-    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let result = retry_with_index(Fixed::from_millis(100), |index| {
+        match TcpListener::bind("127.0.0.1:7878") {
+            Ok(sock) => Ok(sock),
+            Err(e) => Err(format!("Failed {} times: {}", index, e))
+        }
+    });
+
+    let listener = result.unwrap();
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
 
-        printlin!("Connection established");
+        println!("Connection established");
     }
 }
