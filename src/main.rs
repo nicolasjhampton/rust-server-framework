24abd3804918ad1a953a454261221212685cd9b0
Nic Hampton <nicolasjhampton@gmail.com>
2020-06-06 21:29:39 -0700

    Factored out connection

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,17 @@ use std::net::TcpListener;
 use retry::retry_with_index;
 use retry::delay::Fixed;
 
-fn main() {
-    let result = retry_with_index(Fixed::from_millis(100), |index| {
+fn get_conn() -> Result<std::net::TcpListener, retry::Error<String>> {
+    return retry_with_index(Fixed::from_millis(100), |index| {
         match TcpListener::bind("127.0.0.1:7878") {
             Ok(sock) => Ok(sock),
             Err(e) => Err(format!("Failed {} times: {}", index, e))
         }
     });
+}
 
-    let listener = result.unwrap();
+fn main() {
+    let listener = get_conn().unwrap();
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
